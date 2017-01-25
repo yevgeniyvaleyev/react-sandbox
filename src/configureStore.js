f5522d180f4f66b7ec106b19a20cb3cedcc477b3
Yevgeniy Valeyev <webmaster.vea@gmail.com>
2017-01-25 16:15:57 +0100

    3rd party middlewares
    * replaced custom logger and promise middelwares with 3rd party;
    * used 3rd party applier for middlewares;

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,52 +1,25 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
 import todoApp from './reducers';
 
-const promise = (store) => (next) => (action) => { // eslint-disable-line no-unused-vars
-  if (typeof action.then === 'function') {
-    return action.then(next);
-  }
-  return next(action);
-};
-
-// here we set all middlewares with store and dispatch
-// it's used to chain middelwares to execute on by one with preset context
-// Middelwares use curry so curryed function (which accept action) has own next (old dispatch)
-// and store predefined.
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-  middlewares.forEach((middleware) => {
-    store.dispatch = middleware(store)(store.dispatch); // eslint-disable-line no-param-reassign
-  });
-};
-
-const logger = (store) => (next) => {
-  /* eslint-disable no-console */
-  if (!console.group) {
-    return next;
-  }
-
-  return (action) => {
-    console.group(action.type);
-    console.log('%c prev state', 'color: gray', store.getState());
-    console.log('%c action', 'color: blue', action);
-    const returnValue = next(action);
-    console.log('%c next state', 'color: green', store.getState());
-    console.groupEnd(action.type);
-    return returnValue;
-  };
-  /* eslint-enable no-console */
-};
-
+// - Here I use applyMiddleware which applies middlewares providing store and
+// previous dispatch to curried function;
+// 3rd party promise and logger middlewares are used which provide
+// curried function (with store, dispatch)
 const configureStore = () => {
-  const store = createStore(todoApp);
   const middlewares = [];
 
   if (process.env.NODE_ENV !== 'production') {
-    middlewares.push(logger);
+    middlewares.push(createLogger());
   }
   middlewares.push(promise);
 
-  wrapDispatchWithMiddlewares(store, middlewares);
-  return store;
+  // second agrument can be persistedState if needed
+  return createStore(
+    todoApp,
+    applyMiddleware(...middlewares)
+  );
 };
 
 export default configureStore;
